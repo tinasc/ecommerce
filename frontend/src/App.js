accb89d390fe87e8409c909dbc4cfbfec24d6610
Tina <tinasylviac@gmail.com>
2021-10-18 11:26:40 -0400

    Creating Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import data from './data';
+import Product from './components/Product';
+
 const App =()=>{
   return (
     <div className="grid-container">
@@ -18,43 +20,12 @@ const App =()=>{
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product}></Product>
             ))}
           </div>
         </div>
       </main>
-      <footer className="row center">All right reserved</footer>
+      <footer className="row center"> @2021 All right reserved</footer>
     </div>
   );
 }
