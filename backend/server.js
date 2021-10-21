90f4cd9662894c12121d542938b2a2512c4e3b7f
Tina <tinasylviac@gmail.com>
2021-10-20 23:57:29 -0400

    Creating Product Sample in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/ecommerce');
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
