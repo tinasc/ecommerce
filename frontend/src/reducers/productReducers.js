5651ec60ea0a8e84f53ae6b2309270b8a560dd86
Tina <tinasylviac@gmail.com>
2021-10-25 15:57:57 -0400

    Updating Product in Admin

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -9,6 +9,10 @@ import {
     PRODUCT_CREATE_SUCCESS,
     PRODUCT_CREATE_FAIL,
     PRODUCT_CREATE_RESET,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_UPDATE_RESET,
     } from "../constants/productConstants";
 
  export const productListReducer = (
@@ -53,3 +57,17 @@ export const productDetailsReducer = (state = { loading: true }, action) => {
         return state;
     }
   };
+export const productUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true };
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_UPDATE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
