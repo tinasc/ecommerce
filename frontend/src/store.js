dfdbd278eecb750e3da0b9b1d34ef3e64ae348be
Tina <tinasylviac@gmail.com>
2021-10-27 17:08:11 -0400

    Deliver Orders in Admin

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import {
   orderMineListReducer,
   orderListReducer,
   orderDeleteReducer,
+  orderDeliverReducer,
 } from './reducers/orderReducers';
 import {
     productCreateReducer,
@@ -57,6 +58,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   orderList: orderListReducer,
   orderDelete: orderDeleteReducer,
+  orderDeliver: orderDeliverReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
